75c45cbf0e5cdb46991e16bd7166d43236d3cf5f
smitsadharakiya <ksmit1556@gmail.com>
2026-03-12 12:12:45 +0530

    UserDAO.java

--- Lost&FoundManagementSystem._old/src/java/dao/UserDAO.java
+++ Lost&FoundManagementSystem._old/src/java/dao/UserDAO.java
@@ -7,15 +7,14 @@ import model.User;
 
 public class UserDAO {
 
-    // Validate user login and set role
+    
     public static boolean validate(User user) {
         boolean status = false;
 
         try {
-            // Get connection from DBConnection.java
+         
             Connection con = DBConnection.getConnection();
 
-            // Prepare SQL query to check email and password
             PreparedStatement ps = con.prepareStatement(
                 "SELECT * FROM users WHERE email=? AND password=?"
             );
@@ -23,16 +22,16 @@ public class UserDAO {
             ps.setString(1, user.getEmail());
             ps.setString(2, user.getPassword());
 
-            // Execute query
+        
             ResultSet rs = ps.executeQuery();
 
-            // If user exists, set status true and get role
+          
             if (rs.next()) {
                 status = true;
-                user.setRole(rs.getString("role")); // store role in User object
+                user.setRole(rs.getString("role")); 
             }
 
-            // Close resources
+         
             rs.close();
             ps.close();
             con.close();
@@ -44,3 +43,4 @@ public class UserDAO {
         return status;
     }
 }
+
